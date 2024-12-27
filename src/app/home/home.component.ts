6ed6ac5c7d2df9a0c496ae93a5f91cd238d96ba1
jovyzabel <jovymonka7@gmail.com>
2024-12-27 16:33:39 +0100

    #whole-learning-step-3-commits

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,37 @@
+import { Component, inject } from '@angular/core';
+import {HousingLocationComponent} from "../housing-location/housing-location.component";
+import { HousingLocation } from '../housing-location';
+import { HOUSE } from '../mock-housing';
+import { NgFor } from '@angular/common';
+import { HousingService } from '../housing.service';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    HousingLocationComponent,NgFor
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  housingLocationList: HousingLocation[] = []
+  housingService : HousingService = inject(HousingService);
+  filteredLocationList: HousingLocation[] = [];
+  constructor(){
+    //this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.housingService.getAllHousingLocations()
+    .then((housingLocationList: HousingLocation[]) =>{
+      this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList;
+    });
+
+  }
+  filterResults(text: string){
+    if(!text) this.filteredLocationList = this.housingLocationList;
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
+  }
+}
